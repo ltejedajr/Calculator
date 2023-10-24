2052c87b58524bc36766aaa7019c1db84191be38
ltejedajr <126844440+ltejedajr@users.noreply.github.com>
2023-10-23 20:17:54 -0500

    Modified index,html and index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,32 @@
 "use strict;"
 
-console.log("start index.js");
+// console.log("start index.js");
 
 window.onload = init;
 
 function init(){
 
-console.log("start init")
+// console.log("start init")
 
     const addBtn = document.getElementById("addBtn");
     addBtn.onclick = addBtnClicked;
-    
-console.log("end init")
+
+    const subtractBtn = document.getElementById("subtractBtn");
+    subtractBtn.onclick = subtractBtnClicked;
+
+    const multiplyBtn = document.getElementById("multiplyBtn");
+    multiplyBtn.onclick = multiplyBtnClicked;
+
+    const divideBtn = document.getElementById("divideBtn");
+    divideBtn.onclick = divideBtnClicked;
+
+// console.log("end init")
 
 }
 
 function addBtnClicked(){
 
-    console.log("button clicked");
+    // console.log("button clicked");
 
     const number1Field = document.getElementById("number1Field");
     const number2Field = document.getElementById("number2Field");
@@ -25,29 +34,18 @@ function addBtnClicked(){
     let number1 = Number(number1Field.value);
     let number2 = Number(number2Field.value);
 
-    let answer = number1 + number2;
+    let additionAnswer = number1 + number2;
 
     const answerField = document.getElementById("answerField"); 
     
-    answerField.value = answer;
+    answerField.value = additionAnswer;
 
 
 }
 
-function init(){
-
-    console.log("start init")
-    
-        const subtractBtn = document.getElementById("subtractBtn");
-        subtractBtn.onclick = subtractBtnClicked;
-        
-    console.log("end init")
-    
-    }
-
 function subtractBtnClicked(){
 
-    console.log("button clicked");
+    // console.log("button clicked");
 
     const number1Field = document.getElementById("number1Field");
     const number2Field = document.getElementById("number2Field");
@@ -55,11 +53,49 @@ function subtractBtnClicked(){
     let number1 = Number(number1Field.value);
     let number2 = Number(number2Field.value);
 
-    let answer = number1 - number2;
+    let subtractionAnswer = number1 - number2;
 
     const answerField = document.getElementById("answerField"); 
     
-    answerField.value = answer;
+    answerField.value = subtractionAnswer;
+
+
+}
+
+function multiplyBtnClicked(){
+
+    // console.log("button clicked");
+
+    const number1Field = document.getElementById("number1Field");
+    const number2Field = document.getElementById("number2Field");
+
+    let number1 = Number(number1Field.value);
+    let number2 = Number(number2Field.value);
+
+    let multiplicationAnswer = number1 * number2;
+
+    const answerField = document.getElementById("answerField"); 
+    
+    answerField.value = multiplicationAnswer;
+
+
+}
+
+function divideBtnClicked(){
+
+    // console.log("button clicked");
+
+    const number1Field = document.getElementById("number1Field");
+    const number2Field = document.getElementById("number2Field");
+
+    let number1 = Number(number1Field.value);
+    let number2 = Number(number2Field.value);
+
+    let divisionAnswer = number1 / number2;
+
+    const answerField = document.getElementById("answerField"); 
+    
+    answerField.value = divisionAnswer;
 
 
 }
